eee07920f8e50e95e5cf4616598068a3e05f4234
horizonteweb <horizonteweb26@gmail.com>
2025-11-09 11:51:42 -0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://horizonteweb.vercel.app', 
+  siteUrl: 'https://horizonteweb.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ['/server-sitemap.xml'], 
-};
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      'https://horizonteweb.vercel.app/sitemap-0.xml',
+    ],
+  },
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: path === '/' ? 1.0 : 0.7,
+      lastmod: new Date().toISOString(),
+    }
+  },
+}
